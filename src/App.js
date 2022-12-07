fb6555e0bf78847f670eee9a83b1cc403a9b5115
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-07 19:48:16 +0900

    feat(App): 다리 길이 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./domain/BridgeGame');
+const InputView = require('./UI/InputView');
+const OutputView = require('./UI/OutputView');
+const isValidBridgeLength = require('./utils/isValidBridgeLength');
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  play() {
+    OutputView.gameStart();
+    return this.gameSetting();
+  }
+
+  gameSetting() {
+    InputView.readBridgeSize((input) => {
+      try {
+        isValidBridgeLength(input);
+        this.#bridgeGame = new BridgeGame(
+          BridgeMaker(Number(input), BridgeRandomNumberGenerator)
+        );
+        return this.gameEnd();
+      } catch (err) {
+        OutputView.inputLengthError();
+        return this.gameSetting();
+      }
+    });
+  }
+
+  gameEnd() {
+    Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
