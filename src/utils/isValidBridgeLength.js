b7dbdb59023ecd63d080d0e0b89b44b4fe524e2a
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-08 15:32:44 +0900

    refactor(validCheck): validCheck 유틸함수 통합

--- src/utils/isValidBridgeLength.js
+++
@@ -1,6 +0,0 @@
-const isValidBridgeLength = (userInput) => {
-  if (Number.isNaN(Number(userInput))) throw new Error();
-  if (Number(userInput) < 3 || Number(userInput) > 20) throw new Error();
-};
-
-module.exports = isValidBridgeLength;
---
+++ src/utils/validCheck.js
@@ -0,0 +1,8 @@
+const validCheck = {
+  BridgeLength(userInput) {
+    if (Number.isNaN(Number(userInput))) throw new Error();
+    if (Number(userInput) < 3 || Number(userInput) > 20) throw new Error();
+  },
+};
+
+module.exports = validCheck;
