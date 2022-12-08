85c0afbb3bb8b137748e8ff4c286506ab6821124
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-08 15:43:32 +0900

    feat(BridgeGame): BridgeGame domain 추가

--- src/domain/BridgeGame.js
+++ src/domain/BridgeGame.js
@@ -1,12 +1,33 @@
+const { INPUT } = require('../constants/constants');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #initBridge;
+  #answerBridge;
+
   constructor(initBridge) {
     this.#initBridge = initBridge.slice();
+    this.#answerBridge = this.makeAnswerBridge(initBridge);
+    console.log(initBridge);
+    console.log(this.#answerBridge);
   }
 
+  makeAnswerBridge(bridge) {
+    const [Up, Down] = [[], []];
+    bridge.forEach((value) => {
+      if (value === INPUT.UP) {
+        Up.push(INPUT.RIGHT);
+        Down.push(INPUT.WRONG);
+      }
+      if (value === INPUT.DOWN) {
+        Up.push(INPUT.WRONG);
+        Down.push(INPUT.RIGHT);
+      }
+    });
+    return [Up, Down];
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
