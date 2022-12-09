31336731432b2b44df784ed47a67c6e832996d53
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-09 18:04:28 +0900

    feat(getBridgeResult): 다리 이동 결과 유틸함수 추가

---
+++ src/utils/getBridgeResult.js
@@ -0,0 +1,27 @@
+const { INPUT } = require('../constants/constants');
+
+const getBridgeResult = {
+  upFail(nowstate, bridge) {
+    console.log(bridge);
+    const state = Number(nowstate);
+    const result = [bridge[0].slice(0, state - 1), bridge[1].slice(0, state - 1)];
+    result[0][state] = INPUT.WRONG;
+    result[1][state] = INPUT.BLANK;
+    return result;
+  },
+
+  downFail(nowstate, bridge) {
+    const state = Number(nowstate);
+    const result = [bridge[0].slice(0, state), bridge[1].slice(0, state)];
+    result[0][state] = INPUT.BLANK;
+    result[1][state] = INPUT.WRONG;
+    return result;
+  },
+
+  success(state, bridge) {
+    const result = [bridge[0].slice(0, state + 1), bridge[1].slice(0, state + 1)];
+    return result;
+  },
+};
+
+module.exports = getBridgeResult;
